8068b931f2efd7e7742fe21e014a2a618ada4b6b
JeffM20501 <kimmuna440@gmail.com>
2025-12-15 17:19:46 +0300

    completed lab

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,24 @@
 # your code goes here!
+class Anagram:
+    def __init__(self, word):
+        self.word=word
+    
+    @property
+    def word(self):
+        return self._word
+    
+    @word.setter
+    def word(self, value):
+        if isinstance(value, str):
+            self._word=value
+        else:
+            print("Word need to be a string!")
+    
+    def match(self, arr):
+        hold_anagram=[]
+        for anagram in arr:
+            if sorted(self.word)==sorted(anagram):
+                hold_anagram.append(anagram)
+        return hold_anagram
+
+word=Anagram("jeff").match(["effj","man", "jffe"])
